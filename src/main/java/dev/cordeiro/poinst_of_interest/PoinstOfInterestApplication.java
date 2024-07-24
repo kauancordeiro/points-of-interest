efb89085e5ddbb2c5a5dfd8ee085b02696b59588
Kauan <cvitorcordeiro@gmail.com>
2024-07-23 22:58:43 -0300

    First commit

---
+++ src/main/java/dev/cordeiro/poinst_of_interest/PoinstOfInterestApplication.java
@@ -0,0 +1,13 @@
+package dev.cordeiro.poinst_of_interest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoinstOfInterestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoinstOfInterestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/cordeiro/poinst_of_interest/PoinstOfInterestApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cordeiro.poinst_of_interest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoinstOfInterestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
